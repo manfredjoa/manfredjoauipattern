2c8f96c9570d5e67edd96250e958a61a550f0ec8
manfredjoa <joamanfred@gmail.com>
2023-07-04 01:17:30 -0400

    add forEach for fetch request for info

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
-const baseURL = 'https://fruityvice.com/api/fruit/'
-const sections = document.querySelectorAll('.fruit')
+const baseURL = "https://fruityvice.com/api/fruit/"
+const sections = document.querySelectorAll("section")
+
+sections.forEach(section => {
+
+  fetch(`${baseURL}${section.dataset.pageid}`)
+    .then(res => {
+      return res.json()
+
+    }).then(data => {
+      let info = document.createElement("p")
+      info.classList.add("modal")
+
+      info.innerText = `Fruit: ${data.name}
+    Order: ${data.order}
+    Family: ${data.family}
+    Genus: ${data.genus}
+    Calories: ${data.nutritions.calories}
+    Fat: ${data.nutritions.fat}g
+    Sugar: ${data.nutritions.sugar}g
+    Carbohydrates: ${data.nutritions.carbohydrates}g
+    Protein: ${data.nutritions.protein}g`
+
+      section.appendChild(info)
+
+    }).catch(error => {
+      console.error('Error', error)
+    })
+})
