94b2f5d013d8733cabea11efdb5ffdec6c736586
manfredjoa <joamanfred@gmail.com>
2023-07-04 23:25:07 -0400

    add close modal event listeners

--- script.js
+++ script.js
@@ -9,40 +9,29 @@ modals.forEach(modal => {
 
     }).then(data => {
       let info = document.createElement("p")
-      let button = document.createElement("button")
-      // info.classList.add("modal")
 
       info.innerText = `Fruit: ${data.name}
 
-      
     Order: ${data.order}
-
     Family: ${data.family}
-
     Genus: ${data.genus}
-
     Calories: ${data.nutritions.calories}
-
     Fat: ${data.nutritions.fat}g
-
     Sugar: ${data.nutritions.sugar}g
-
     Carbohydrates: ${data.nutritions.carbohydrates}g
-
     Protein: ${data.nutritions.protein}g`
 
       modal.appendChild(info)
-
-      button.innerText = 'x'
-      modal.appendChild(button)
 
     }).catch(error => {
       console.error('Error', error)
     })
 })
 
+
 const fruits = document.querySelectorAll(".fruit")
 const overlay = document.querySelector(".overlay")
+const buttons = document.querySelectorAll("button")
 
 for (let i = 0; i < modals.length; i++) {
   if (fruits[i].id === modals[i].id) {
@@ -54,11 +43,24 @@ for (let i = 0; i < modals.length; i++) {
   }
 }
 
-const buttons = document.querySelectorAll("button")
+const closeModal = () => {
+  for (let i = 0; i < modals.length; i++) {
+    modals[i].classList.add("hidden")
+    overlay.classList.add("hidden")
+  }
+}
 
-// buttons.forEach(button => {
-//   button.addEventListener("click", {
-//     overlay.classList.add("hidden")
-//   })
-// })
-// Not sure about the syntax error here with '.'
+buttons.forEach(button => {
+  button.addEventListener("click", closeModal)
+})
+
+overlay.addEventListener("click", closeModal)
+
+document.addEventListener("keydown", (e) => {
+  for (let i = 0; i < modals.length; i++) {
+
+    if (e.key === "Escape" && !modals[i].classList.contains("hidden")) {
+      closeModal()
+    }
+  }
+})
